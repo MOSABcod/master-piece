906fcaa65a55acfc45d0a6998853ea90ca128112
SamerAljbour <samerseran34@gmail.com>
2024-12-01 15:48:19 +0300

    added logout and icons in dashborad remaining show the result of each student after taking the test and the road map middleware

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -1,4 +1,3 @@
-<!-- Sidebar Start -->
 <div class="sidebar pe-4 pb-3">
     <nav class="navbar bg-light navbar-light">
         <a href="index.html" class="navbar-brand mx-4 mb-3">
@@ -6,6 +5,7 @@
         </a>
         <div class="d-flex align-items-center ms-4 mb-4">
             <div class="position-relative">
+                {{-- Profile Image Placeholder --}}
                 {{-- <img class="rounded-circle" src="img/user.jpg" alt="" style="width: 40px; height: 40px;"> --}}
                 {{-- <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div> --}}
             </div>
@@ -29,13 +29,20 @@
             </a>
 
             <a href="{{ route('viewTeachers') }}" class="nav-item nav-link {{ Request::routeIs('viewTeachers') ? 'active' : '' }}">
-                <i class="fa fa-th me-2"></i>ادارة المعلمات
+                <i class="fa fa-chalkboard-teacher me-2"></i>إدارة المعلمات
             </a>
 
             <a href="{{ route('viewStudents') }}" class="nav-item nav-link {{ Request::routeIs('viewStudents') ? 'active' : '' }}">
-                <i class="fa fa-th me-2"></i>ادارة الطلاب
+                <i class="fa fa-users me-2"></i>إدارة الطلاب
             </a>
+
+            <!-- Logout -->
+            <form action="{{ route('logout') }}" method="POST" class="nav-item nav-link p-0">
+                @csrf
+                <button type="submit" style="margin-right: 8px !important " class="btn btn-link text-decoration-none text-dark w-100 text-end">
+                    <i class="fa fa-sign-out-alt me-2"></i>تسجيل خروج
+                </button>
+            </form>
         </div>
     </nav>
 </div>
-<!-- Sidebar End -->
